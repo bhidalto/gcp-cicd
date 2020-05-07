3199ef177135060b612a0c40d5f8c4a8dc4836ee
Borja <borjah@google.com>
2020-05-07 17:41:47 +0200

    Pushing files to dev

---
+++ dev/hello_world/main.py
@@ -0,0 +1,18 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World! This is our development environment!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
